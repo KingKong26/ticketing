d374eaaef9e386ce0b442077b1a2a36e6b22dff0
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-01 17:30:58 +0530

    new changes for deployment

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server side render
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "https://www.ticketing.vishnuverse.xyz/",
       headers: req.headers,
     });
   } else {
